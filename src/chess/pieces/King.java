64d992248dbadc7332a41b6dfd87b6fb3c5733a6
ThiagoBD <thiagobarrosdias@gmail.com>
2024-06-19 09:50:04 -0300

    Logic piece move 1

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -4,12 +4,19 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece {
+public abstract class King extends ChessPiece {
 
     public King(Board board, Color color) {
         super(board, color);
 
     }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
+
     @Override
     public String toString(){
         return "K";
